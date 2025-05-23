d4a7dec58d968ed9799647168725993d063c8e10
tabetant <antoine.tabet@mail.utoronto.ca>
2025-05-23 12:57:26 +0300

    navbar additional styling

--- src/app/ui/navbar.jsx
+++ src/app/ui/navbar.jsx
@@ -5,23 +5,31 @@ import Link from 'next/link'
 export default function NavBar() {
     const pathname = usePathname();
     return (
-        <nav className='flex flex-row justify-center text-black'>
-            <div>
+        <nav className='flex my-5 text-2xl bg-blue-500 flex-row justify-around text-white font-bold'>
+            <div className={pathname == '/about' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
                 <Link
-                    className={pathname === '/about' ? 'hover:text-violet-600 mx-2 bg-blue-100' : 'hover:text-violet-600 mx-2'}
+                    className='hover:text-black mx-2'
                     href='/about'
                 >
                     About
                 </Link>
-            </div>
-            <div>
+            </div >
+            <div className={pathname == '/experience' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
                 <Link
+                    className='hover:text-black mx-2'
+                    href='/experience'
+                >
+                    Experience & Awards
+                </Link>
+            </div >
+            <div className={pathname == '/projects' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
+                <Link
+                    className='hover:text-black mx-2'
                     href='/projects'
-                    className={pathname === '/projects' ? 'hover:text-violet-600 mx-2 bg-blue-100' : 'hover:text-violet-600 mx-2'}
                 >
                     Projects
                 </Link>
-            </div>
-        </nav>
+            </div >
+        </nav >
     )
 }
