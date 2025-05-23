507eb1b03764d927614f01094e144d7354bde5e2
tabetant <antoine.tabet@mail.utoronto.ca>
2025-05-23 13:40:19 +0300

    draft work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,51 @@
 export default function EduPage() {
     return (
-        <h1 className='text-center text-black'>My Work Experiences</h1>
+        <div className='text-black' >
+            <h1 className='text-center text-black'>My Work Experiences</h1>
+            <div id="tutoring">
+                <h4>Private Tutor, <em>Tutor <div>April 2023 - Present</div></em></h4>
+                <ul>
+                    <li>Provided one-on-one tutoring for high school students in mathematics, physics, and
+                        chemistry, improving their academic performance</li>
+                    <li>Developed customized study plans and problem-solving strategies, helping students excel
+                        in official examinations</li>
+                    <li>Compiled comprehensive study materials to reinforce understanding and simplify complex
+                    </li>
+                </ul>
+            </div>
+            <div id="HMUN24">
+                <h4>Harvard MUN Internship with The Council Leadership Academy, <em>Advisor<div>July 2023 -
+                    January 2024</div></em></h4>
+                <ul>
+                    <li>Selected to train new delegates for Harvard MUN 2024, coaching them in public speaking,
+                        research, and diplomatic negotiations</li>
+                    <li>Led weekly training sessions on conference procedures, resolution drafting, and policy
+                        research</li>
+                    <li>Reviewed and provided detailed feedback on position papers and speeches, ensuring
+                        high-quality submissions</li>
+                </ul>
+            </div>
+            <div id="CVS">
+                <h4>Colonie de Vacances Souraty, <em>Tutor<div>July 2024 & 2025 - August 2024 & 2025</div></em>
+                </h4>
+                <ul>
+                    <li>Worked for two weeks at a Ceméa-certified summer camp in Faraya, Lebanon.</li>
+                    <li>In each week, led a group of 10 kids through various activities (sports, DIY projects,
+                        bonfires).</li>
+                    <li>Learned how to lead, work and communicate with children; and how to handle crises in
+                        high pressure.</li>
+                </ul>
+            </div>
+            <div id="Kaplan">
+                <h4>Test Prep Institute (Certified by Kaplan), <em>SAT Assistant Instructor<div>August 2023
+                </div></em></h4>
+                <ul>
+                    <li>Supported instructors with teaching SAT Mathematics and English intensive classes to
+                        high schoolers (i.e., answered questions during problem-solving sessions, led solutions
+                        sessions post-SAT practice tests, graded practice tests).</li>
+                    <li>Held additional online office hours for students.</li>
+                </ul>
+            </div>
+        </div >
     )
 }
