5922737790f03d9864e7597afb81b48f6a454559
tabetant <antoine.tabet@mail.utoronto.ca>
2025-05-23 13:23:13 +0300

    added content to education and awards

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,5 +1,42 @@
 export default function EduPage() {
     return (
-        <h1 className='text-center text-black'>My Education</h1>
+        <>
+            <div className="text-black">
+                <h1>Education</h1>
+                <hr />
+                <h4>
+                    <strong>Collège Louise Wegmann, Badaro, Lebanon</strong>
+                </h4>
+                <div>
+                    <p>
+                        French Baccalaureate Candidate; Specialty in: Mathematics, Physics and Chemistry, English.
+                        Overall GPA: 18.7/20 (Academic Year 2023-2024); Ranking: <strong>2/90</strong>
+                    </p>
+                </div>
+                <h4>
+                    <strong>University of Toronto, Toronto, Canada</strong>
+                </h4>
+                <div>
+                    <p>
+                        Bachelor’s of Applied Science in Electrical and Computer Engineering + PEY Co-op
+                        Relevant Courses: Computer Fundamentals, Electrical Fundamentals, Introduction to Electrical
+                        and
+                        Computer Engineering
+                        GPA: <strong>3.77/4.0</strong> | Dean’s List, Academic Year 2024-2025
+                    </p>
+                </div>
+            </div>
+            <div className='text-black'>
+                <h1>Awards</h1>
+                <hr />
+                <ul>
+                    <li><strong>“Excellence Prize”</strong> Collège Louise Wegmann, 2024</li>
+                    <li><strong>“Outstanding Delegate Award”</strong> Loudoun International Youth Leadership Summit,
+                        2023
+                    </li>
+                    <li><strong>“Diplomatic Commendation”</strong> Harvard Model United Nations, 2023</li>
+                </ul>
+            </div>
+        </>
     )
 }
