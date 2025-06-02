c1772d61225670812e0c023f74b4cdc8973d5549
tabetant <antoine.tabet@mail.utoronto.ca>
2025-06-02 10:01:58 +0300

    navbar styling

--- src/app/ui/navbar.jsx
+++ src/app/ui/navbar.jsx
@@ -5,42 +5,42 @@ import Link from 'next/link'
 export default function NavBar() {
     const pathname = usePathname();
     return (
-        <nav className='flex my-5 text-2xl bg-blue-500 flex-row justify-around text-white font-bold'>
-            <div className={pathname == '/about' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
+        <nav className='flex rounded-full shadow px-6 py-2 my-5 text-2xl bg-blue-500 flex-row justify-around text-white font-bold'>
+            <div className={pathname == '/about' ? 'w-1/8 bg-blue-200 rounded-full' : 'w-1/8 bg-blue-500'}>
                 <Link
-                    className='hover:text-black mx-2'
+                    className='hover:text-black mx-2 font-bold space-x-6'
                     href='/about'
                 >
                     About
                 </Link>
             </div >
-            <div className={pathname == '/education' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
+            <div className={pathname == '/education' ? 'w-1/8 bg-blue-200 rounded-full' : 'w-1/8 bg-blue-500'}>
                 <Link
-                    className='hover:text-black mx-2'
+                    className='hover:text-black mx-2 font-bold space-x-6'
                     href='/education'
                 >
                     Education & Awards
                 </Link>
             </div >
-            <div className={pathname == '/projects' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
+            <div className={pathname == '/projects' ? 'w-1/8 bg-blue-200 rounded-full' : 'w-1/8 bg-blue-500'}>
                 <Link
-                    className='hover:text-black mx-2'
+                    className='hover:text-black mx-2 font-bold space-x-6'
                     href='/projects'
                 >
                     Projects
                 </Link>
             </div >
-            <div className={pathname == '/leadership' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
+            <div className={pathname == '/leadership' ? 'w-1/8 bg-blue-200 rounded-full' : 'w-1/8 bg-blue-500'}>
                 <Link
-                    className='hover:text-black mx-2'
+                    className='hover:text-black mx-2 font-bold space-x-6'
                     href='/leadership'
                 >
                     Leadership and Volunteering
                 </Link>
             </div >
-            <div className={pathname == '/work' ? 'w-1/8 bg-blue-200' : 'w-1/8 bg-blue-500'}>
+            <div className={pathname == '/work' ? 'w-1/8 bg-blue-200 rounded-full' : 'w-1/8 bg-blue-500'}>
                 <Link
-                    className='hover:text-black mx-2'
+                    className='hover:text-black mx-2 font-bold space-x-6'
                     href='/work'
                 >
                     Work
